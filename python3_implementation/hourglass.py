ae84ea58a824483836abf8e01858d2f6245ba703
eunicecompra <eunicecompra@gmail.com>
2019-12-10 23:19:34 +1100

    Correctly named y and x axes

--- python3_implementation/hourglass.py
+++ python3_implementation/hourglass.py
@@ -1,29 +1,29 @@
 def find_max_sum_hourglass(array_2d):
     sums = []
     array_len = len(array_2d)
-    x_index = 0
-    for x_axis in array_2d:
-        x_len = len(x_axis)
-        y_index = 0
-        for y_axis in x_axis:
-            if y_index+2 >= x_len:
+    y_index = 0
+    for y_axis in array_2d:
+        y_len = len(y_axis)
+        x_index = 0
+        for x_axis in y_axis:
+            if x_index+2 >= y_len:
                 continue
-            top = y_axis + x_axis[y_index+1] + x_axis[y_index+2]
+            top = x_axis + y_axis[x_index+1] + y_axis[x_index+2]
 
-            if x_index+1 >= array_len:
+            if y_index+1 >= array_len:
                 continue
-            middle = array_2d[x_index+1][y_index+1]
+            middle = array_2d[y_index+1][x_index+1]
 
-            if x_index+2 >= array_len:
+            if y_index+2 >= array_len:
                 continue
-            bottom_x = array_2d[x_index+2]
-            bottom = bottom_x[y_index] + bottom_x[y_index+1] + bottom_x[y_index+2]
+            bottom_x = array_2d[y_index+2]
+            bottom = bottom_x[x_index] + bottom_x[x_index+1] + bottom_x[x_index+2]
 
             sum = top + middle + bottom
             sums.append(sum)
-            y_index += 1
+            x_index += 1
 
-        x_index += 1
+        y_index += 1
 
     sums.sort()
     return sums[len(sums)-1]
