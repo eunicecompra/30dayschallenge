a2dbb3a92d0081dcbcfb945aa44fff1d36fd618f
eunicecompra <eunicecompra@gmail.com>
2019-12-10 23:13:29 +1100

    Fixed edge case bug by using the correct index to access array

--- python3_implementation/hourglass.py
+++ python3_implementation/hourglass.py
@@ -4,22 +4,25 @@ def find_max_sum_hourglass(array_2d):
     x_index = 0
     for x_axis in array_2d:
         x_len = len(x_axis)
+        y_index = 0
         for y_axis in x_axis:
-            if y_axis+2 >= x_len:
+            if y_index+2 >= x_len:
                 continue
-            top = x_axis[y_axis] + x_axis[y_axis+1] + x_axis[y_axis+2]
+            top = y_axis + x_axis[y_index+1] + x_axis[y_index+2]
 
             if x_index+1 >= array_len:
                 continue
-            middle = array_2d[x_index+1][y_axis+1]
+            middle = array_2d[x_index+1][y_index+1]
 
             if x_index+2 >= array_len:
                 continue
             bottom_x = array_2d[x_index+2]
-            bottom = bottom_x[y_axis] + bottom_x[y_axis+1] + bottom_x[y_axis+2]
+            bottom = bottom_x[y_index] + bottom_x[y_index+1] + bottom_x[y_index+2]
 
             sum = top + middle + bottom
             sums.append(sum)
+            y_index += 1
+
         x_index += 1
 
     sums.sort()
@@ -31,3 +34,4 @@ if __name__ == '__main__':
 
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
+    print(find_max_sum_hourglass(arr))
