1de562f8325b199dabeab57b81d6ee5e41aaf92e
eunicecompra <eunicecompra@gmail.com>
2020-01-08 22:20:53 +1100

    Implemented nested while loop to keep on iterating until elements are all swapped

--- python3_implementation/src/bubblesort.py
+++ python3_implementation/src/bubblesort.py
@@ -6,13 +6,19 @@ n = int(input().strip())
 a = list(map(int, input().strip().split(' ')))
 
 def do_bubble_sort(size, array):
-    for i in range(size):
-        if i+1 == size:
-            break
+    swapped = True
 
-        if array[i] > array[i+1]:
-            tmp = array[i]
-            array[i] = array[i+1]
-            array[i+1] = tmp
+    while(swapped):
+        swapped = False
+        for i in range(size):
+            if i+1 == size:
+                break
+
+            if array[i] > array[i+1]:
+                tmp = array[i]
+                array[i] = array[i+1]
+                array[i+1] = tmp
+                swapped = True
+
     return array
 
